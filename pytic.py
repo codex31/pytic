024192bf36ac9a572d56956c16ea868adaf5b5e7
codex31 <kemashidayat83@gmail.com>
2019-10-26 19:59:07 +0700

    First commit

---
+++ pytic.py
@@ -0,0 +1,38 @@
+#!/data/data/com.termux/files/usr/bin/python
+
+import statistics
+from os import system
+
+try:
+    lis = []
+    print("-" * 50)
+    data = str(input("Masukkan data: "))
+    datalist = data.split(",")
+    for i in datalist:
+        lis.append(int(i))
+
+    mean = statistics.mean(lis)
+    median = statistics.median(lis)
+    modus = statistics.mode(lis)
+    sd = statistics.stdev(lis)
+    varian = statistics.variance(lis)
+    Xmax = max(lis)
+    Xmin = min(lis)
+    rangenya = Xmax-Xmin
+
+    trash = "HASIL"
+    trash = trash.center(50, " ")
+    print("-" * 50)
+    print(trash)
+    print("-" * 50)
+    print("Jumlah Data    : {}". format(len(lis)))
+    print("Mean           : {}". format(mean))
+    print("Median         : {}". format(median))
+    print("Modus          : {}". format(modus))
+    print("Standar Deviasi: {}". format(sd))
+    print("Varian         : {}". format(varian))
+    print("Range          : {}". format(rangenya))
+
+    
+except KeyboardInterrupt:
+    print("CTRL-C Pressed, exiting...")
